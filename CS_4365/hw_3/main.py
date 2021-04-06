962554c7d7f10b5d5219dd6410ea510fbd5a239b
spenpal2000 <sanjeev.penupala@gmail.com>
2021-04-05 19:31:10 -0500

    chore: create main.py template

---
+++ CS_4365/hw_3/main.py
@@ -0,0 +1,35 @@
+# Filename:     main.py
+# Date:         4/5/21
+# Authors:      Sanjeev Penupala, Sanjana Sivakumar
+# Email:        sanjeev.penupala@utdallas.edu, sxs170002@utdallas.edu
+# Course:       CS 4365.001
+#
+# Description:
+#
+#       Implement a theorem prover fora clause logic using the resolution principle
+#
+
+###########
+# IMPORTS #
+###########
+
+# Standard Library Imports
+
+
+
+#############
+# FUNCTIONS #
+#############
+
+
+
+########
+# MAIN #
+########
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
